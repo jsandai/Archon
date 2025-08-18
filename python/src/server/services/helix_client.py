c7706eae4cf28f4d658fd9797e7943581cf04d06
jsandai <130126800+jsandai@users.noreply.github.com>
2025-08-18 09:20:43 -0500

    Update helix_client.py
     Fixed import statement, updated client configuration

--- python/src/server/services/helix_client.py
+++ python/src/server/services/helix_client.py
@@ -5,20 +5,26 @@ Manages HelixDB client connections.
 """
 
 import os
-from helix_py import HelixDB
+from helix import Client
 from ..config.logfire_config import search_logger
 
-def get_helix_client() -> HelixDB:
+def get_helix_client() -> Client:
     """
     Get a HelixDB client instance.
 
     Returns:
-        HelixDB client instance
+        Client: HelixDB client instance (helix.Client)
     """
     # The host of the helixdb service is `helixdb` as defined in docker-compose.yml
     # The default port is 6969
     try:
-        client = HelixDB(host="helixdb")
+        # For Docker container connections, we need to use the service name as hostname
+        # Since we're connecting to another container, local=False and use api_endpoint
+        client = Client(
+            local=False, 
+            api_endpoint="http://helixdb:6969",
+            verbose=True
+        )
         search_logger.info("HelixDB client initialized")
         return client
     except Exception as e:
